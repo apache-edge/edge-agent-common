dc9af1d4aa89884d1a1c36889c5d9b5e9a8a7684
Robbert Brandsma <i@robbertbrandsma.nl>
2025-04-04 12:26:29 +0200

    Add edge agent common code

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version:6.1
+import PackageDescription
+
+let package = Package(
+    name: "edge-agent-common",
+    platforms: [
+        .macOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "EdgeAgentGRPC",
+            targets: ["EdgeAgentGRPC"]
+        ),
+        .library(
+            name: "Shell",
+            targets: ["Shell"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "2.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.6.3"),
+    ],
+    targets: [
+        .target(
+            name: "EdgeAgentGRPC",
+            dependencies: [
+                .product(name: "GRPCCore", package: "grpc-swift"),
+                .product(name: "GRPCProtobuf", package: "grpc-swift-protobuf"),
+            ],
+            exclude: [
+                "Proto/edge_agent.protoset"
+            ]
+        ),
+        .target(
+            name: "Shell",
+            dependencies: [
+                .product(name: "Logging", package: "swift-log")
+            ]
+        ),
+    ]
+)
